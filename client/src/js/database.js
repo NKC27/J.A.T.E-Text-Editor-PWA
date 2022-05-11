8104a77d731f94bfd54df747fcc85c45cb73c297
NKC27 <93439579+NKC27@users.noreply.github.com>
2022-05-11 16:40:46 +0100

    js folder code update,src-sw update

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,4 +1,6 @@
-import { openDB } from 'idb';
+import {
+  openDB
+} from 'idb';
 
 const initdb = async () =>
   openDB('jate', 1, {
@@ -7,15 +9,36 @@ const initdb = async () =>
         console.log('jate database already exists');
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      db.createObjectStore('jate', {
+        keyPath: 'id',
+        autoIncrement: true
+      });
       console.log('jate database created');
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const jateDB = await openDB("jate", 1);
+  const tx = jateDB.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
+  const request = store.put({
+    jate: content
+  });
+  const result = await request;
+  console.log("The data has been saved to the database", result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  {
+    const jateDB = await openDB("jate", 1);
+    const tx = jateDB.transaction("jate", "readwrite");
+    const store = tx.objectStore("jate");
+    const req = store.getAll();
+    const result = await req;
+    console.log(result);
+  };
+}
 
 initdb();
